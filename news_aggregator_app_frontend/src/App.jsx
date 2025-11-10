7ec43b2700a71ab6f60817564d27cd4b4855c8ff
Kamwerudroid <mmakweru@gmail.com>
2025-11-10 08:51:11 +0300

    Components added to App.jsx

--- news_aggregator_app_frontend/src/App.jsx
+++ news_aggregator_app_frontend/src/App.jsx
@@ -1,35 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// App.js
+import React from "react";
+import NavBar2 from "./Components/NavBar2";
+import NavBar1 from "./Components/NavBar1";
+import News from "./Components/News";
+import { BrowserRouter, Route, Routes } 
+	from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
+	return (
+		<>
+			<div className="App">
+				<BrowserRouter>
+					<NavBar1 />
+					<NavBar2 />
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+					<div className="container">
+						<div className="row">
+							<div className="col-md">
+								<Routes>
+									<Route
+										path="/"
+										element={
+											<News key="general"
+											category="general" />}
+									/>
+									<Route
+										path="/Entertainment"
+										element={
+											<News key="entertainment"
+											category="entertainment" />
+										}
+									/>
+									<Route
+										path="/Technology"
+										element={
+											<News key="technology"
+											category="technology" />}
+									/>
+									<Route
+										path="/Sports"
+										element={
+											<News key="sports"
+											category="sports" />}
+									/>
+									<Route
+										path="/Business"
+										element={
+											<News key="business"
+											category="business" />}
+									/>
+									<Route
+										path="/Health"
+										element={
+											<News key="health"
+											category="health" />}
+									/>
+									<Route
+										path="/Science"
+										element={
+											<News key="science"
+											category="science" />}
+									/>
+								</Routes>
+							</div>
+						</div>
+					</div>
+				</BrowserRouter>
+			</div>
+		</>
+	);
 }
 
-export default App
+export default App;
